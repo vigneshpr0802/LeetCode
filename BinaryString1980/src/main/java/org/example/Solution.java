79954f9a8d9296678f95b00fab76516a11be8ae0
Vignesh <vigneshpr0802@gmail.com>
2023-11-16 13:19:42 +0530

    BinaryString problem

---
+++ BinaryString1980/src/main/java/org/example/Solution.java
@@ -0,0 +1,13 @@
+package org.example;
+
+class Solution {
+    public String findDifferentBinaryString(String[] nums) {
+        StringBuilder sb = new StringBuilder();
+        for (int i=0; i< nums.length; i++) {
+           sb.append(nums[i].charAt(i) == '1' ? "0": "1");
+
+        }
+        return  sb.toString();
+        
+    }
+}
